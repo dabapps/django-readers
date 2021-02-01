c421b7dbc04cb0da223df553981e8a4b19fbd088
Jamie Matthews <jamie@dabapps.com>
2021-02-01 16:10:28 +0000

    Add SQL-level tests for various prefetching functions

--- tests/test_qs.py
+++ tests/test_qs.py
@@ -2,7 +2,8 @@ from django.db import connection
 from django.test import TestCase
 from django.test.utils import CaptureQueriesContext
 from djunc import qs
-from tests.models import Owner, Widget
+from tests.models import Category, Owner, Widget
+from unittest import mock
 
 
 class QuerySetTestCase(TestCase):
@@ -62,3 +63,140 @@ class QuerySetTestCase(TestCase):
 
         with self.assertNumQueries(0):
             self.assertEqual(widgets[0].owner.name, "test owner")
+
+    def test_prefetch_forward_relationship(self):
+        Widget.objects.create(
+            name="test widget", owner=Owner.objects.create(name="test owner")
+        )
+
+        prepare = qs.prefetch_forward_relationship(
+            "owner", qs.include_fields("name")(Owner.objects.all())
+        )
+
+        with CaptureQueriesContext(connection) as capture:
+            widgets = list(prepare(Widget.objects.all()))
+
+        self.assertEqual(len(capture.captured_queries), 2)
+
+        self.assertEqual(
+            capture.captured_queries[0]["sql"],
+            "SELECT "
+            '"tests_widget"."id", '
+            '"tests_widget"."owner_id" '
+            "FROM "
+            '"tests_widget"',
+        )
+
+        self.assertEqual(
+            capture.captured_queries[1]["sql"],
+            "SELECT "
+            '"tests_owner"."id", '
+            '"tests_owner"."name" '
+            "FROM "
+            '"tests_owner" '
+            "WHERE "
+            '"tests_owner"."id" IN (1)',
+        )
+
+        with self.assertNumQueries(0):
+            self.assertEqual(widgets[0].owner.name, "test owner")
+
+    def test_prefetch_reverse_relationship(self):
+        Widget.objects.create(
+            name="test widget", owner=Owner.objects.create(name="test owner")
+        )
+
+        prepare = qs.pipe(
+            qs.include_fields("name"),
+            qs.prefetch_reverse_relationship(
+                "widget_set", "owner", qs.include_fields("name")(Widget.objects.all())
+            ),
+        )
+
+        with CaptureQueriesContext(connection) as capture:
+            owners = list(prepare(Owner.objects.all()))
+
+        self.assertEqual(len(capture.captured_queries), 2)
+
+        self.assertEqual(
+            capture.captured_queries[0]["sql"],
+            "SELECT "
+            '"tests_owner"."id", '
+            '"tests_owner"."name" '
+            "FROM "
+            '"tests_owner"',
+        )
+
+        self.assertEqual(
+            capture.captured_queries[1]["sql"],
+            "SELECT "
+            '"tests_widget"."id", '
+            '"tests_widget"."name", '
+            '"tests_widget"."owner_id" '
+            "FROM "
+            '"tests_widget" '
+            "WHERE "
+            '"tests_widget"."owner_id" IN (1)',
+        )
+
+        with self.assertNumQueries(0):
+            self.assertEqual(owners[0].widget_set.all()[0].name, "test widget")
+
+    def test_prefetch_many_to_many_relationship(self):
+        widget = Widget.objects.create(name="test widget")
+        category = Category.objects.create(name="test category")
+
+        widget.category_set.add(category)
+
+        prepare = qs.pipe(
+            qs.include_fields("name"),
+            qs.prefetch_many_to_many_relationship(
+                "category_set", qs.include_fields("name")(Category.objects.all())
+            ),
+        )
+
+        with CaptureQueriesContext(connection) as capture:
+            widgets = list(prepare(Widget.objects.all()))
+
+        self.assertEqual(len(capture.captured_queries), 2)
+
+        self.assertEqual(
+            capture.captured_queries[0]["sql"],
+            "SELECT "
+            '"tests_widget"."id", '
+            '"tests_widget"."name" '
+            "FROM "
+            '"tests_widget"',
+        )
+
+        self.assertEqual(
+            capture.captured_queries[1]["sql"],
+            "SELECT "
+            '("tests_category_widget_set"."widget_id") '
+            'AS "_prefetch_related_val_widget_id", '
+            '"tests_category"."id", '
+            '"tests_category"."name" '
+            "FROM "
+            '"tests_category" '
+            "INNER JOIN "
+            '"tests_category_widget_set" ON '
+            '("tests_category"."id" = "tests_category_widget_set"."category_id") '
+            "WHERE "
+            '"tests_category_widget_set"."widget_id" IN (1)',
+        )
+
+        with self.assertNumQueries(0):
+            self.assertEqual(widgets[0].category_set.all()[0].name, "test category")
+
+    def test_auto_prefetch_relationship(self):
+        with mock.patch("djunc.qs.prefetch_forward_relationship") as mock_fn:
+            qs.auto_prefetch_relationship("owner")(Widget.objects.all())
+            mock_fn.assert_called_once()
+
+        with mock.patch("djunc.qs.prefetch_reverse_relationship") as mock_fn:
+            qs.auto_prefetch_relationship("thing")(Widget.objects.all())
+            mock_fn.assert_called_once()
+
+        with mock.patch("djunc.qs.prefetch_many_to_many_relationship") as mock_fn:
+            qs.auto_prefetch_relationship("category_set")(Widget.objects.all())
+            mock_fn.assert_called_once()
