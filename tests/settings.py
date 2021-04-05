ac44c91d73eb1d2b15d18edea578239feffb602f
Jamie Matthews <jamie@dabapps.com>
2021-04-05 12:40:52 +0100

    Add Django 3.2rc1 to test matrix and add new setting to suppress warning

--- tests/settings.py
+++ tests/settings.py
@@ -3,3 +3,5 @@ DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": ":memor
 INSTALLED_APPS = ["tests"]
 
 SECRET_KEY = "abcde12345"
+
+DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
